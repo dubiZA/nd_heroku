97f2437158a02ccfd9c6ed3846fd1fd4d9888088
dubiZA <iamdzh@outlook.com>
2020-07-16 10:04:45 -0600

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export DATABASE_URL="postgres://dpfomwhtneszja:f31268070c00ba9e1c7c28514bfc4df60481eceef2b11dc5b0fb2b6ff8a3a55b@ec2-54-91-178-234.compute-1.amazonaws.com:5432/dacm9vdbha1lfi"
